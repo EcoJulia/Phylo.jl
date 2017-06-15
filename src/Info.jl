4cca6c0e143ad499bc1b54dd4cd83f5dc9a61a6a
Richard Reeve <git@richardreeve.net>
2017-06-15 22:30:16 +0100

    Remove irrelevant code

--- src/Info.jl
+++ src/Info.jl
@@ -14,24 +14,15 @@ _setheight!(li::LeafInfo, height::Float64) = (li.height = height)
 function _hasheight(::AbstractInfo)
     return false
 end
-function _hasheight(::Void)
-    return false
-end
+
 #  - _getheight() - should never be called as hasheight returns false
 function _getheight(::AbstractInfo)
     throw(NullException())
     return NaN
 end
-function _getheight(::Void)
-    throw(NullException())
-    return NaN
-end
+
 #  - _setheight!() - ignore set value
 function _setheight!(::AbstractInfo, value::Float64)
-    warn("Ignoring height")
-    return value
-end
-function _setheight!(::Void, value::Float64)
     warn("Ignoring height")
     return value
 end
