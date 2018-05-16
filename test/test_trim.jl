96804dbc007a27a1b44fae2a66a034851b10f994
Claire Harris <c.harris.2@research.gla.ac.uk>
2018-05-16 13:49:51 +0100

    test_trim.jl: Add missing end to Module TestTrim, and update testing of droptips!() so that it now includes keeptips!().

--- test/test_trim.jl
+++ test/test_trim.jl
@@ -10,16 +10,24 @@ using Compat.Test
     tips = getleafnames(test_tree)
     root = collect(nodenamefilter(isroot, test_tree))
     @test length(ints ∩ tips) == 0
-    @test length(ints ∩ root) == 0 
+    @test length(ints ∩ root) == 0
 end
 
-@testset "drop_tip!()" begin
+@testset "keeptips!()" begin
     # Create a 10 tip tree
     test_tree = rand(Ultrametric(10))
-    keep_tips = ["tip 1", "tip 2", "tip 3"]
+    keep_tips = ["tip 5", "tip 6", "tip 7", "tip 8", "tip 9", "tip 10"]
     # Drop all but first three tips
-    drop_tip!(test_tree, keep_tips)
+    keeptips!(test_tree, keep_tips)
     tips = getleafnames(test_tree)
+    test_tree2 = rand(Ultrametric(10))
+    drop_tips = ["tip 1", "tip 2", "tip 3", "tip 4"]
+    droptips!(test_tree2, drop_tips)
+    tips2 = getleafnames(test_tree2)
     @test Set(tips) == Set(keep_tips)
+    @test Set(tips) == Set(tips2)
     @test validate(test_tree)
+    @test validate(test_tree2)
+end
+
 end
