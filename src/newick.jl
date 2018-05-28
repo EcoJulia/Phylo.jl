4ca30f196ae628c9d96709fc1e6ac1a0fc8cab37
Richard Reeve <git@richardreeve.net>
2018-05-28 04:24:20 +0100

    newick.jl: Fixes to allow for - and + before numbers, and anonymous nodes.

--- src/newick.jl
+++ src/newick.jl
@@ -56,10 +56,17 @@ function checktosemi(test::Function, token, state, tokens)
     return true
 end
 
-function parsevector(token, state, tokens, ::Type{TY}) where TY <: Real
+function parsevector(token, state, tokens, ::Type{TY}, sgn) where TY <: Real
     vec = TY[]
     while token.kind != T.RBRACE && token.kind != T.ENDMARKER
-        push!(vec, parse(TY, untokenize(token)))
+        if token.kind == T.PLUS
+            sgn = +;
+            token, state = nextskip(tokens, state)
+        elseif token.kind == T.MINUS
+            sgn = -;
+            token, state = nextskip(tokens, state)
+        end
+        push!(vec, sgn(parse(TY, untokenize(token))))
         token, state = nextskip(tokens, state)
         if token.kind != T.COMMA && token.kind != T.RBRACE
             tokenerror(token, ",")
@@ -67,16 +74,25 @@ function parsevector(token, state, tokens, ::Type{TY}) where TY <: Real
             token, state = nextskip(tokens, state)
         end
     end
+    
     return token, state, vec
 end
 
 function parsevector(token, state, tokens)
     token, state = nextskip(tokens, state)
 
+    sgn = +;
+    if token.kind == T.MINUS
+        sgn = -;
+        token, state = nextskip(tokens, state)
+    elseif token.kind == T.PLUS
+        token, state = nextskip(tokens, state)
+    end        
+    
     if token.kind == T.INTEGER
-        return parsevector(token, state, tokens, Int)
+        return parsevector(token, state, tokens, Int, sgn)
     elseif token.kind == T.FLOAT
-        return parsevector(token, state, tokens, Float64)
+        return parsevector(token, state, tokens, Float64, sgn)
     end
 
     vec = String[]
@@ -93,16 +109,19 @@ function parsevector(token, state, tokens)
             token, state = nextskip(tokens, state)
         end
     end
+    
     return token, state, vec
 end
 
 function parsedict(token, state, tokens)
     dict = Dict{String, Any}()
-    token, state = next(tokens, state)
+    token, state = nextskip(tokens, state)
     if token.kind != T.AND
         tokenerror(token, "&")
+    else
+        token, state = nextskip(tokens, state)
     end
-    token, state = nextskip(tokens, state)
+    
     while token.kind != T.RSQUARE && token.kind != T.ENDMARKER
         token, state, key = tokensgetkey(token, state, tokens, isEQorRSQUARE)
         if token.kind != T.RSQUARE # Allow [&R] as a valid (empty) dict
@@ -110,35 +129,49 @@ function parsedict(token, state, tokens)
             if token.kind == T.LBRACE
                 token, state, value = parsevector(token, state, tokens)
             else
+                sgn = +;
+                if token.kind == T.PLUS
+                    sgn = +;
+                    token, state = nextskip(tokens, state)
+                elseif token.kind == T.MINUS
+                    sgn = -;
+                    token, state = nextskip(tokens, state)
+                end
+
                 if token.kind == T.INTEGER
-                    value = parse(Int, untokenize(token))
+                    value = sgn(parse(Int, untokenize(token)))
                 elseif token.kind == T.FLOAT
-                    value = parse(Float64, untokenize(token))
+                    value = sgn(parse(Float64, untokenize(token)))
                 elseif token.kind ∈ [T.STRING, T.CHAR]
                     value = untokenize(token)[2:end-1]
                 else
                     value = untokenize(token)
                 end
             end
-            dict[key]=value
+            dict[key] = value
+            
             token, state = nextskip(tokens, state)
             if token.kind != T.COMMA && token.kind != T.RSQUARE
-                tokenerror(token, ",")
+                tokenerror(token, ", or ]")
             elseif token.kind == T.COMMA
                 token, state = nextskip(tokens, state)
             end
         end
+    end
+    
+    if token.kind == T.RSQUARE
+        token, state = nextskip(tokens, state)
     end
 
     return token, state, dict
 end
 
 function parsenode(token, state, tokens, tree::TREE,
-                   lookup, siblings, istip) where {NL, BL, TREE <: AbstractBranchTree{NL, BL}}
+                   lookup, siblings, istip) where {NL, BL,
+                                                   TREE <: AbstractBranchTree{NL, BL}}
     myname = ""
     endkinds = [T.SEMICOLON, T.COLON, T.COMMA, T.RPAREN, T.LSQUARE]
-    if token.kind ∉ endkinds
-        # We got a nodename
+    if token.kind ∉ endkinds # We got a nodename
         token, state, name = tokensgetkey(token, state, tokens,
                                           t -> t.kind ∈ endkinds)
         if isempty(lookup)
@@ -166,29 +199,40 @@ function parsenode(token, state, tokens, tree::TREE,
         end
         myname = addnode!(tree)
     end
-    siblings[myname] = Dict{String, Any}()
+
     foundcolon = false
     if token.kind == T.COLON
         foundcolon = true
         token, state = nextskip(tokens, state)
     end
+
+    siblings[myname] = Dict{String, Any}()
     if token.kind == T.LSQUARE
         token, state, dict = parsedict(token, state, tokens)
         siblings[myname]["dict"] = dict
         setnoderecord!(tree, myname, dict)
-        token, state = nextskip(tokens, state)
     end
+
     if token.kind == T.COLON || foundcolon
         if token.kind == T.COLON
             token, state = nextskip(tokens, state)
         end
+        sgn = +;
+        if token.kind == T.PLUS
+            sgn = +;
+            token, state = nextskip(tokens, state)
+        elseif token.kind == T.MINUS
+            sgn = -;
+            token, state = nextskip(tokens, state)
+        end
         if token.kind ∈ [T.INTEGER, T.FLOAT]
-            siblings[myname]["length"] = parse(Float64, untokenize(token))
+            siblings[myname]["length"] = sgn(parse(Float64, untokenize(token)))
         else
             tokenerror(token, "a length")
         end
         token, state = nextskip(tokens, state)
     end
+
     return token, state, myname
 end
 
@@ -196,14 +240,12 @@ function parsenewick(token, state, tokens, tree::TREE,
                      lookup = Dict(), depth = 0,
                      children = Dict{NL, Dict{String, Any}}()) where
     {NL, BL, TREE <: AbstractBranchTree{NL, BL}}
-    token, state = nextskip(tokens, state)
     mychildren = Dict{NL, Dict{String, Any}}()
     while token.kind != T.RPAREN && token.kind != T.ENDMARKER
+        token, state = nextskip(tokens, state)
         if token.kind == T.LPAREN
             token, state = parsenewick(token, state, tokens, tree,
                                        lookup, depth + 1, mychildren)
-        elseif token.kind == T.COMMA
-            token, state = nextskip(tokens, state)
         else
             token, state, nodename = parsenode(token, state, tokens, tree,
                                                lookup, mychildren, true)
@@ -334,7 +376,6 @@ function parsetrees(token, state, tokens,
         addnodes!(trees[treename], collect(values(taxa)))
         if token.kind == T.LSQUARE
             token, state, treedata[treename] = parsedict(token, state, tokens)
-            token, state = nextskip(tokens, state)
         end
         if !isEQ(token)
             tokenerror(token, "=")
@@ -342,7 +383,6 @@ function parsetrees(token, state, tokens,
             token, state = nextskip(tokens, state)
             if token.kind == T.LSQUARE
                 token, state, _ = parsedict(token, state, tokens)
-                token, state = nextskip(tokens, state)
             end
         end
         if token.kind != T.LPAREN
