4ac1a7f86c9a4d541058d53116848589a5e277db
Richard Reeve <git@richardreeve.net>
2018-07-01 18:29:26 +0100

    Allow different label types for TreeSets, so they can be indexed by number.

--- src/TreeSet.jl
+++ src/TreeSet.jl
@@ -4,14 +4,14 @@ import Compat.IteratorSize, Base.length, Compat.IteratorEltype, Base.eltype
 
 import Phylo.API: _nodetype, _branchtype
 
-mutable struct TreeSet{NL, BL, TREE <: AbstractTree{NL, BL}} <: AbstractTree{NL, BL}
-    trees::Dict{String, TREE}
+mutable struct TreeSet{LABEL, NL, BL, TREE <: AbstractTree{NL, BL}} <: AbstractTree{NL, BL}
+    trees::Dict{LABEL, TREE}
     treeinfo::Dict{String, Dict{String, Any}}
 end
 
-TreeSet(trees::Dict{String, TREE}, treeinfo) where
-        {NL, BL, TREE <: AbstractTree{NL, BL}} =
-    TreeSet{NL, BL, TREE}(trees, treeinfo)
+TreeSet(trees::Dict{LABEL, TREE}, treeinfo) where
+        {LABEL, NL, BL, TREE <: AbstractTree{NL, BL}} =
+    TreeSet{LABEL, NL, BL, TREE}(trees, treeinfo)
 
 function IteratorSize(::Type{TreeSet})
     return HasLength()
@@ -23,35 +23,35 @@ end
 
 _nodetype(ts::TreeSet) = _nodetype(first(treeiter(ts)))
 _branchtype(ts::TreeSet) = _branchtype(first(treeiter(ts)))
-_branchtype(::TreeSet{NL, BL, TREE}) where {NL, BL, TREE <: AbstractBranchTree{NL, BL}} = Branch{NL}
+_branchtype(::TreeSet{LABEL, NL, BL, TREE}) where {LABEL, NL, BL, TREE <: AbstractBranchTree{NL, BL}} = Branch{NL}
 ntrees(ts::TreeSet) = length(ts.trees)
 
-struct TreeIterator{NL, BL, TREE <: AbstractTree{NL, BL},
-                    TREESET <: TreeSet{NL, BL, TREE}} <: AbstractTreeIterator{TREE}
+struct TreeIterator{LABEL, NL, BL, TREE <: AbstractTree{NL, BL},
+                    TREESET <: TreeSet{LABEL, NL, BL, TREE}} <: AbstractTreeIterator{TREE}
     ts::TREESET
 end
-treeiter(ts::TREESET) where {NL, BL, TREE <: AbstractTree{NL, BL},
-                    TREESET <: TreeSet{NL, BL, TREE}} =
+treeiter(ts::TREESET) where {LABEL, NL, BL, TREE <: AbstractTree{NL, BL},
+                    TREESET <: TreeSet{LABEL, NL, BL, TREE}} =
     TreeIterator{NL, BL, TREE, TREESET}(ts)
 
-struct TreeNameIterator{NL, BL, TREE <: AbstractTree{NL, BL},
-                    TREESET <: TreeSet{NL, BL, TREE}} <: AbstractTreeIterator{TREE}
+struct TreeNameIterator{LABEL, NL, BL, TREE <: AbstractTree{NL, BL},
+                    TREESET <: TreeSet{LABEL, NL, BL, TREE}} <: AbstractTreeIterator{TREE}
     ts::TREESET
 end
-treenameiter(ts::TREESET) where {NL, BL, TREE <: AbstractTree{NL, BL},
-                    TREESET <: TreeSet{NL, BL, TREE}} =
+treenameiter(ts::TREESET) where {LABEL, NL, BL, TREE <: AbstractTree{NL, BL},
+                    TREESET <: TreeSet{LABEL, NL, BL, TREE}} =
     TreeNameIterator{NL, BL, TREE, TREESET}(ts)
 
-struct TreeInfoIterator{NL, BL, TREE <: AbstractTree{NL, BL},
-                    TREESET <: TreeSet{NL, BL, TREE}} <: AbstractTreeIterator{TREE}
+struct TreeInfoIterator{LABEL, NL, BL, TREE <: AbstractTree{NL, BL},
+                    TREESET <: TreeSet{LABEL, NL, BL, TREE}} <: AbstractTreeIterator{TREE}
     ts::TREESET
 end
-treeinfoiter(ts::TREESET) where {NL, BL, TREE <: AbstractTree{NL, BL},
-                    TREESET <: TreeSet{NL, BL, TREE}} =
+treeinfoiter(ts::TREESET) where {LABEL, NL, BL, TREE <: AbstractTree{NL, BL},
+                    TREESET <: TreeSet{LABEL, NL, BL, TREE}} =
     TreeInfoIterator{NL, BL, TREE, TREESET}(ts)
 
-eltype(::TreeSet{NL, BL, TREE}) where {NL, BL, TREE <: AbstractTree{NL, BL}} = TREE
-eltype(::TreeIterator{NL, BL, TREE, TREESET}) where {NL, BL, TREE <: AbstractTree{NL, BL}, TREESET <: TreeSet{NL, BL, TREE}} = TREE
+eltype(::TreeSet{LABEL, NL, BL, TREE}) where {LABEL, NL, BL, TREE <: AbstractTree{NL, BL}} = TREE
+eltype(::TreeIterator{LABEL, NL, BL, TREE, TREESET}) where {LABEL, NL, BL, TREE <: AbstractTree{NL, BL}, TREESET <: TreeSet{LABEL, NL, BL, TREE}} = TREE
 eltype(::TreeNameIterator) = String
 eltype(::TreeInfoIterator) = Dict{String, Any}
 
