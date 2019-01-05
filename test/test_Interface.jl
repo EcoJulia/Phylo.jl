b41f238babbe126f38c2e99dac7041aea37a9564
Richard Reeve <git@richardreeve.net>
2019-01-05 19:14:02 +0000

    Start fixing tests to new interface.

--- test/test_Interface.jl
+++ test/test_Interface.jl
@@ -6,26 +6,25 @@ using Compat.Test
 
 @testset "Build and tear down trees" begin
     @testset "For $TreeType" for TreeType in
-        [NamedTree,
-         BinaryTree{DataFrame, Vector{Float64}},
-         BinaryTree{DataFrame, Vector{String}}]
+        [NamedTree, NamedBinaryTree,
+         BinaryTree{ManyRoots, DataFrame, Vector{Float64}}]
 
         species = ["Dog", "Cat", "Human", "Potato", "Apple"]
         tree = TreeType(species)
         othernodes = ["Some 1", "Some 2"]
-        @test othernodes == addnodes!(tree, othernodes)
-        extra = addnodes!(tree, 1)
+        @test othernodes == createnodes!(tree, othernodes)
+        extra = createnodes!(tree, 1)
         @test isa(extra[1], String)
         append!(othernodes, extra)
-        also = addnode!(tree)
+        also = createnode!(tree)
         @test also ∉ othernodes
-        @test_throws ErrorException addnode!(tree, also)
-        @test_throws ErrorException addnodes!(tree, [also])
+        @test_throws ErrorException createnode!(tree, also)
+        @test_throws ErrorException createnodes!(tree, [also])
         @test_throws ErrorException deletenode!(tree, "really not")
         @test also == deletenode!(tree, also)
-        also = addnode!(tree)
+        also = createnode!(tree)
         push!(othernodes, also)
-        @test Set(othernodes) ⊆ Set(nodenameiter(tree))
+        @test Set(othernodes) ⊆ Set(getnodenames(tree))
         innodes = append!(copy(species), othernodes)
         allnodes = reverse(innodes)
         pop!(innodes)
@@ -34,11 +33,11 @@ using Compat.Test
                 itr = Iterators.filter(name ->
                                        hasoutboundspace(tree, name) &&
                                        name != node, allnodes)
-                addbranch!(tree, first(itr), node)
+                createbranch!(tree, first(itr), node)
             end
-        @test Set(branches) == Set(branchnameiter(tree))
+        @test Set(branches) == Set(getbranchnames(tree))
         @test validate(tree)
-        @test_throws ErrorException addbranch!(tree, allnodes[1], allnodes[2])
+        @test_throws ErrorException createbranch!(tree, allnodes[1], allnodes[2])
         who = getparent(tree, species[1])
         b = getinbound(tree, species[1])
         node1 = getnode(tree, species[1])
@@ -57,30 +56,28 @@ using Compat.Test
             indegree(tree, species[1]) == 0
         @test_throws ErrorException getbranch(tree, b)
         branches = collect(Iterators.filter(name -> name != b, branches))
-        @test Set(branches) == Set(branchnameiter(tree))
+        @test Set(branches) == Set(getbranchnames(tree))
         b3 = getinbound(tree, species[2])
         source = src(tree, b3)
         destination = dst(tree, b3)
-        @test b3 == changesrc!(tree, b3, who)
-        @test b3 == changedst!(tree, b3, species[1])
-        b2 = addbranch!(tree, source, destination)
+        @test b3 == deletebranch!(tree, b3)
+        b3 = createbranch!(tree, who, species[1])
+        @test who == src(tree, b3)
+        @test species[1] == dst(tree, b3)
+        b2 = createbranch!(tree, source, destination)
         push!(branches, b2)
-        @test Set(branches) == Set(branchnameiter(tree))
+        @test Set(branches) == Set(getbranchnames(tree))
         @test species[1] == deletenode!(tree, species[1])
         @test !hasnode(tree, species[1])
-        @test species[1] == branch!(tree, who, destination=species[1])
+        @test species[1] == createnode!(tree, species[1])
+        @test createbranch!(tree, who, species[1]) isa Int
         @test hasnode(tree, species[1])
         @test validate(tree)
-        @test all(map(==, Pair(first(branchiter(tree))),
-                      Tuple(tree, first(branchnameiter(tree)))))
-        @test all(map(==, Pair(tree, first(branchnameiter(tree))),
-                      Tuple(first(branchiter(tree)))))
-        @test all(map(node -> isleaf(tree, node), species))
-        @test all(map(node -> !isroot(tree, node) &&
-                      !isunattached(tree, node) &&
-                      !isinternal(tree, node), species))
-        @test Set(map(pair -> src(pair[2]), collect(getbranches(tree)))) ∪
-            Set(map(pair -> dst(pair[2]), collect(getbranches(tree)))) ==
+        @test all(isleaf(tree, node) for node in species)
+        @test all((!isroot(tree, node) & !isunattached(tree, node) &
+                   !isinternal(tree, node)) for node in species)
+        @test Set(src(tree, branch) for branch in getbranches(tree)) ∪
+            Set(dst(tree, branch) for branch in getbranches(tree)) ==
             Set(allnodes)
     end
 end
