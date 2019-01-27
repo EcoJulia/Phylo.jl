74948f1d27d645f845d8cbd30abf06b4c2f6a596
Richard Reeve <git@richardreeve.net>
2019-01-27 19:09:44 +0000

    Updating tests of interface and iterators.

--- test/test_Interface.jl
+++ test/test_Interface.jl
@@ -13,42 +13,46 @@ using Compat.Test
         species = ["Dog", "Cat", "Human", "Potato", "Apple"]
         tree = TreeType(species)
         othernodes = ["Some 1", "Some 2"]
-        @test othernodes == createnodes!(tree, othernodes)
-        extra = createnodes!(tree, 1)
+        nodes = createnodes!(tree, othernodes)
+        @test [getnodename(tree, node) for node in nodes] == othernodes
+        extra = [getnodename(tree, node) for node in createnodes!(tree, 1)]
         @test isa(extra[1], String)
         append!(othernodes, extra)
-        also = createnode!(tree)
+        also = getnodename(tree, createnode!(tree))
         @test also ∉ othernodes
         @test_throws ErrorException createnode!(tree, also)
         @test_throws ErrorException createnodes!(tree, [also])
         @test_throws ErrorException deletenode!(tree, "really not")
-        @test also == deletenode!(tree, also)
-        also = createnode!(tree)
+        @test deletenode!(tree, also)
+        also = getnodename(tree, createnode!(tree))
         push!(othernodes, also)
         @test Set(othernodes) ⊆ Set(getnodenames(tree))
         innodes = append!(copy(species), othernodes)
         allnodes = reverse(innodes)
         pop!(innodes)
-        branches =
-            map(innodes) do node
-                itr = Iterators.filter(name ->
-                                       hasoutboundspace(tree, name) &&
-                                       name != node, allnodes)
-                createbranch!(tree, first(itr), node)
-            end
-        @test Set(branches) == Set(getbranchnames(tree))
+        @test_throws AssertionError getroot(tree)
+        branches = map(innodes) do node
+            itr = Iterators.filter(name -> hasoutboundspace(tree, name) &&
+                                   name != node, allnodes)
+            getbranch(tree, createbranch!(tree, first(itr), node))
+        end
+        @test_nowarn getroot(tree)
+        branchnames = [getbranchname(tree, branch) for branch in branches]
+        @test Set(branchnames) == Set(getbranchnames(tree))
         @test validate!(tree)
         @test_throws ErrorException createbranch!(tree, allnodes[1], allnodes[2])
         who = getparent(tree, species[1])
-        b = getinbound(tree, species[1])
+        b = getbranch(tree, getinbound(tree, species[1]))
+        bn = getbranchname(tree, b)
         node1 = getnode(tree, species[1])
         @test !isunattached(tree, node1) &&
             hasoutboundspace(tree, node1) && !hasinboundspace(tree, node1) &&
             outdegree(tree, node1) == 0 && indegree(tree, node1) == 1
         @test hasbranch(tree, b)
-        @test dst(getbranch(tree, b)) == species[1]
-        @test src(getbranch(tree, b)) == who
-        @test b == deletebranch!(tree, b)
+        @test hasbranch(tree, bn)
+        @test getnodename(tree, dst(tree, getbranch(tree, b))) == species[1]
+        @test getnodename(tree, src(tree, getbranch(tree, b))) == who
+        @test deletebranch!(tree, b)
         @test !hasbranch(tree, b)
         @test isunattached(tree, species[1]) &&
             hasoutboundspace(tree, species[1]) &&
@@ -56,30 +60,37 @@ using Compat.Test
             outdegree(tree, species[1]) == 0 &&
             indegree(tree, species[1]) == 0
         @test_throws ErrorException getbranch(tree, b)
-        branches = [name for name in branches if name != b]
+        branches = [branch for branch in branches if branch != b]
         @test Set(branches) == Set(getbranches(tree))
         b3 = getinbound(tree, species[2])
         source = src(tree, b3)
         destination = dst(tree, b3)
-        @test b3 == deletebranch!(tree, b3)
+        @test deletebranch!(tree, b3)
+        branches = [branch for branch in branches if branch != b3]
         b3 = createbranch!(tree, who, species[1])
-        @test who == src(tree, b3)
-        @test species[1] == dst(tree, b3)
-        b2 = createbranch!(tree, source, destination)
-        push!(branches, b2)
-        @test Set(branches) == Set(getbranchnames(tree))
-        @test species[1] == deletenode!(tree, species[1])
+        @test who == getnodename(tree, src(tree, b3))
+        spparent = getparent(tree, getnode(tree, species[1]))
+        @test getnode(tree, spparent) == getnode(tree, src(tree, b3))
+        @test species[1] == getnodename(tree, dst(tree, b3))
+        b2 = getbranchname(tree, createbranch!(tree, source, destination))
+        branchnames = [getbranchname(tree, branch) for branch in branches]
+        push!(branchnames, b2, getbranchname(tree, b3))
+        @test Set(branchnames) == Set(getbranchnames(tree))
+        @test deletenode!(tree, species[1])
         @test !hasnode(tree, species[1])
-        @test species[1] == createnode!(tree, species[1])
-        @test createbranch!(tree, who, species[1]) isa Int
+        @test species[1] == getnodename(tree, createnode!(tree, species[1]))
+        @test_nowarn createbranch!(tree, who, species[1])
         @test hasnode(tree, species[1])
         @test validate!(tree)
         @test all(isleaf(tree, node) for node in species)
         @test all((!isroot(tree, node) & !isunattached(tree, node) &
                    !isinternal(tree, node)) for node in species)
-        @test Set(src(tree, branch) for branch in getbranches(tree)) ∪
-            Set(dst(tree, branch) for branch in getbranches(tree)) ==
-            Set(allnodes)
+        @test Set(getnodename(tree, src(tree, branch))
+                  for branch in getbranches(tree)) ∪
+            Set(getnodename(tree, dst(tree, branch))
+                for branch in getbranches(tree)) == Set(getnodenames(tree))
+        createnode!(tree)
+        @test (roottype(TreeType) == OneRoot) ⊻ validate!(tree)
     end
 end
 
--- test/test_Iterators.jl
+++ test/test_Iterators.jl
@@ -10,26 +10,26 @@ using Compat
 
     nni = nodenameiter(nur)
     @test Compat.IteratorEltype(nni) == Base.HasEltype()
-    @test eltype(nni) == nodenametype(nur)
+    @test eltype(nni) == nodenametype(typeof(nur))
     @test Compat.IteratorSize(nni) == Base.HasLength()
     @test length(nni) == 2 * ntip - 1
 
     ni = nodeiter(nur)
     @test Compat.IteratorEltype(ni) == Base.HasEltype()
-    @test eltype(ni) == nodetype(nur)
+    @test eltype(ni) == nodetype(typeof(nur))
     @test Compat.IteratorSize(ni) == Base.HasLength()
     @test length(ni) == 2 * ntip - 1
     @test Set(ni) == Set(map(n -> getnode(nur, n), nni))
 
     bni = branchnameiter(nur)
     @test Compat.IteratorEltype(bni) == Base.HasEltype()
-    @test eltype(bni) == branchnametype(nur)
+    @test eltype(bni) == branchnametype(typeof(nur))
     @test Compat.IteratorSize(bni) == Base.HasLength()
     @test length(bni) == ntip * 2 - 2
 
     bi = branchiter(nur)
     @test Compat.IteratorEltype(bi) == Base.HasEltype()
-    @test eltype(bi) == branchtype(nur)
+    @test eltype(bi) == branchtype(typeof(nur))
     @test Compat.IteratorSize(bi) == Base.HasLength()
     @test length(bi) == 2 * ntip - 2
     @test Set(bi) == Set(map(b -> getbranch(nur, b), bni))
@@ -39,38 +39,38 @@ end
     ntip = 10
     nur = rand(TreeType(ntip))
 
-    nni = nodenamefilter(isleaf, nur)
+    nni = nodenamefilter(n -> isleaf(nur, n), nur)
     @test Compat.IteratorEltype(nni) == Base.HasEltype()
-    @test eltype(nni) == nodenametype(nur)
+    @test eltype(nni) == nodenametype(typeof(nur))
     @test Compat.IteratorSize(nni) == Base.HasLength()
     @test length(nni) == ntip
     leaves = collect(nni)
-    @test leaves == getleafnames(nur)
+    @test Set(leaves) == Set(getleafnames(nur))
 
-    ni = nodefilter(isleaf, nur)
+    ni = nodefilter(n -> isleaf(nur, n), nur)
     @test Compat.IteratorEltype(ni) == Base.HasEltype()
-    @test eltype(ni) == nodetype(nur)
+    @test eltype(ni) == nodetype(typeof(nur))
     @test Compat.IteratorSize(ni) == Base.HasLength()
     @test length(ni) == ntip
-    @test all(isleaf, collect(ni))
+    @test all(n -> isleaf(nur, n), collect(ni))
 
     bni = branchnamefilter(nur) do branch
-        return isleaf(nur, dst(branch))
+        return isleaf(nur, dst(nur, branch))
     end
     @test Compat.IteratorEltype(bni) == Base.HasEltype()
-    @test eltype(bni) == branchnametype(nur)
+    @test eltype(bni) == branchnametype(typeof(nur))
     @test Compat.IteratorSize(bni) == Base.HasLength()
     @test length(bni) == ntip
 
     bi = branchfilter(nur) do branch
-        return isleaf(nur, dst(branch))
+        return isleaf(nur, dst(nur, branch))
     end
     @test Compat.IteratorEltype(bi) == Base.HasEltype()
-    @test eltype(bi) == branchtype(nur)
+    @test eltype(bi) == branchtype(typeof(nur))
     @test Compat.IteratorSize(bi) == Base.HasLength()
     @test length(bi) == ntip
-    bileaves = map(dst, bi)
-    @test Set(bileaves) == Set(leaves)
+    bileaves = map(b -> dst(bi.tree, b), bi)
+    @test Set(bileaves) == Set(getnode.(nur, leaves))
 end
 
 end
