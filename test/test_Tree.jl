f35395d95c1b5ddf15fdec3bc9413fd2a76781b6
Richard Reeve <git@richardreeve.net>
2019-02-22 07:51:48 +0300

    Reincorporate JuliaDB

--- test/test_Tree.jl
+++ test/test_Tree.jl
@@ -2,7 +2,7 @@ module TestTrees
 
 using Phylo
 using DataFrames
-#using JuliaDB
+using JuliaDB
 
 using Compat.Test
 using IterableTables: getiterator
@@ -11,78 +11,98 @@ species = ["Dog", "Cat", "Human"]
 ntips = 10
 df = DataFrame(species = species, count = [10, 20, 3])
 observations = ["Dog", "Cat", "Dog", "Dog"]
-#jdb = table(@NT(species = observations, count = 1:4))
-jdb = DataFrame(species = observations, count = 1:4)
-
-@testset "NamedTree()" begin
-    ntn = NamedTree(ntips)
+@static if VERSION < v"0.7.0-"
+    jdb = table(@NT(species = observations, count = 1:4))
+else
+    jdb = table((species = observations, count = 1:4))
+end
+@testset "NamedBinaryTree()" begin
+    ntn = NamedBinaryTree(ntips)
     @test length(nodefilter(isroot, ntn)) == ntips
     @test length(nodefilter(isleaf, ntn)) == ntips
     @test length(nodefilter(isinternal, ntn)) == 0
     @test validate!(ntn)
-    nt = NamedTree(species)
-    @test_throws ErrorException noderoute(nt, "Dog", "Cat")
-    @test_throws ErrorException branchroute(nt, "Dog", "Human")
+    nt = NamedBinaryTree(species)
+    @test_throws Exception noderoute(nt, "Dog", "Cat")
+    @test_throws Exception branchroute(nt, "Dog", "Human")
     @test validate!(nt)
-    n = addnode!(nt)
-    b1 = addbranch!(nt, n, "Dog", 2.0)
-    b2 = addbranch!(nt, n, "Cat", 2.0)
-    @test_throws ErrorException addbranch!(nt, n, "Human", 2.0)
+    n = getnodename(nt, createnode!(nt))
+    b1 = createbranch!(nt, n, "Dog", 2.0)
+    b2 = createbranch!(nt, n, "Cat", 2.0)
+    @test_throws Exception createbranch!(nt, n, "Human", 2.0)
     @test validate!(nt)
-    r = addnode!(nt)
-    @test_throws ErrorException addbranch!(nt, r, "Potato", 2.0)
-    b3 = addbranch!(nt, r, "Human", 5.0)
-    b4 = addbranch!(nt, r, n, 3.0)
+    r = getnodename(nt, createnode!(nt))
+    @test_throws Exception createbranch!(nt, r, "Potato", 2.0)
+    b3 = createbranch!(nt, r, "Human", 5.0)
+    b4 = createbranch!(nt, r, n, 3.0)
     @test maximum(distances(nt)) ≈ 10.0
     @test validate!(nt)
     @test noderoute(nt, "Human", "Dog") == ["Human", r, n, "Dog"]
     @test branchroute(nt, "Human", "Dog") == [b3, b4, b1]
+    nb = BinaryTree{OneRoot}(DataFrame(name=["Human", "Cat"]))
+    @test_throws Exception BinaryTree(nb)
     nb = BinaryTree(DataFrame(name=["Human", "Cat"]))
+    @test_throws Exception BinaryTree(nb)
+    nb = BinaryTree(DataFrame(name=["Human"]))
+    @test_nowarn BinaryTree(nb)
+    nb = BinaryTree{ManyRoots}(DataFrame(name=["Human", "Cat"]))
     @test getleafinfo(nb) === getleafinfo(BinaryTree(nb))
     @test getleafinfo(nb) !== getleafinfo(BinaryTree(nb; copyinfo = true))
-    @test_nowarn addnode!(nb, "Dog")
+    @test_nowarn createnode!(nb, "Dog")
     if VERSION < v"0.7.0-"
         @test_warn "LeafInfo names do not match actual leaves of tree" !validate!(nb) || error("validate!() should have returned false")
     else
         @test !validate!(nb)
     end
+    np = PolytomousTree{OneRoot}(DataFrame(name=["Human", "Cat"]))
+    @test_throws Exception PolytomousTree(np)
     np = PolytomousTree(DataFrame(name=["Human", "Cat"]))
-    @test getleafinfo(np) === getleafinfo(PolytomousTree(np))
+    @test_throws Exception BinaryTree(np)
+    np = PolytomousTree(DataFrame(name=["Human"]))
+    @test_nowarn PolytomousTree(np)
+    np = PolytomousTree{ManyRoots}(DataFrame(name=["Human", "Cat"]))
     @test getleafinfo(np) !== getleafinfo(PolytomousTree(np; copyinfo = true))
-    @test_nowarn addnode!(np, "Dog")
+    @test_nowarn createnode!(np, "Dog")
     if VERSION < v"0.7.0-"
         @test_warn "LeafInfo names do not match actual leaves of tree" !validate!(np) || error("validate!() should have returned false")
     else
         @test !validate!(np)
     end
-    @test_throws ErrorException PolytomousTree(np)
+    @test_throws Exception PolytomousTree(np)
     @test getleafnames(nb) ⊆ getleafnames(np)
     @test getleafinfo(BinaryTree(df)) == df
     @test getleafinfo(PolytomousTree(df)) == df
 end
 
 @testset "BinaryTree()" begin
-    btn = BinaryTree{DataFrame, Vector{Float64}}(ntips)
+    btn = BinaryTree{OneRoot, DataFrame, Vector{Float64}}(ntips)
     @test length(nodefilter(isroot, btn)) == ntips
     @test length(nodefilter(isleaf, btn)) == ntips
     @test length(nodefilter(isinternal, btn)) == 0
 
-    tmp = BinaryTree(DataFrame(names=species))
+    tmp = BinaryTree{ManyRoots}(DataFrame(names=species))
     @test validate!(tmp)
-    @test_nowarn addbranch!(tmp, addnode!(tmp), species[1])
+    tmp = BinaryTree(DataFrame(names=species))
+    @test !validate!(tmp)
+    names = createnodes!(tmp, 2)
+    @test_nowarn createbranch!(tmp, names[1], species[1])
+    @test_nowarn createbranch!(tmp, names[1], species[2])
+    @test_nowarn createbranch!(tmp, names[2], species[3])
+    @test_nowarn createbranch!(tmp, names[2], names[1])
     @test Set(getleafnames(tmp)) == Set(species)
-    nt = BinaryTree{DataFrame, Vector{Float64}}(species)
+    @test validate!(tmp)
+    nt = BinaryTree{ManyRoots, DataFrame, Vector{Float64}}(species)
     @test validate!(nt)
-    n = addnode!(nt)
+    n = getnodename(nt, createnode!(nt))
     @test Set(getleafnames(nt)) == Set(species) ∪ Set([n])
-    addbranch!(nt, n, "Dog", 2.0)
-    addbranch!(nt, n, "Cat", 2.0)
-    @test_throws ErrorException addbranch!(nt, n, "Human", 2.0)
+    createbranch!(nt, n, "Dog", 2.0)
+    createbranch!(nt, n, "Cat", 2.0)
+    @test_throws Exception createbranch!(nt, n, "Human", 2.0)
     @test validate!(nt)
-    r = addnode!(nt)
-    @test_throws ErrorException addbranch!(nt, r, "Potato", 2.0)
-    addbranch!(nt, r, "Human", 4.0)
-    addbranch!(nt, r, n, 2.0)
+    r = getnodename(nt, createnode!(nt))
+    @test_throws Exception createbranch!(nt, r, "Potato", 2.0)
+    createbranch!(nt, r, "Human", 4.0)
+    createbranch!(nt, r, n, 2.0)
     @test validate!(nt)
     setnodedata!(nt, "Dog", [1.0])
     @test getnodedata(nt, "Dog")[1] ≈ 1.0
@@ -91,12 +111,13 @@ end
     @test length(getnodedata(nt2, "Dog")) == 0
     @test Set(getleafnames(nt2)) == Set(species)
     @test validate!(nt)
-    nt3 = BinaryTree(nt, empty=false)
+    nt3 = BinaryTree(nt)
+    nt4 = BinaryTree(nt3)
     a=IOBuffer()
     b=IOBuffer()
-    show(a, nt)
-    show(b, nt3)
-    @test a.data == b.data
+    show(a, nt3)
+    show(b, nt4)
+    @test all(a.data .== b.data)
 
     tdf = BinaryTree(df)
     @test nleaves(tdf) == 3
