bb00671dd013857e1c53d7a14f82b22400eb150f
Richard Reeve <git@richardreeve.net>
2019-02-22 13:44:10 +0300

    Fix tests to new API

--- test/test_routes.jl
+++ test/test_routes.jl
@@ -5,10 +5,12 @@ using Compat.Test
 
 @testset "routes" begin
     species = ["Dog", "Cat", "Human", "Potato"]
-    tree = NamedTree(species)
-    nr = addnode!(tree)
-    n2 = branch!(tree, nr)
-    n3 = branch!(tree, n2)
+    tree = RootedTree(species)
+    nr = createnode!(tree)
+    n2 = createnode!(tree)
+    createbranch!(tree, nr, n2)
+    n3 = createnode!(tree)
+    createbranch!(tree, n2, n3)
     nh = nodehistory(tree, n2)
     @test nr ∈ nh
     @test Set(nh) == Set(push!(getancestors(tree, n2), n2))
@@ -18,16 +20,16 @@ using Compat.Test
     @test n3 ∈ getdescendants(tree, nr)
     @test n3 ∉ getchildren(tree, nr)
     @test Set([nr, n2, n3]) == Set(nodehistory(tree, n3))
-    br = addbranch!(tree, n3, "Dog")
+    br = getbranchname(tree, createbranch!(tree, n3, "Dog"))
     @test br ∈ branchhistory(tree, "Dog")
-    br2 = addbranch!(tree, n3, "Cat")
+    br2 = getbranchname(tree, createbranch!(tree, n3, "Cat"))
     @test [br2, br] == branchroute(tree, "Cat", "Dog")
-    br3 = addbranch!(tree, n2, "Human")
-    @test addbranch!(tree, nr, "Potato", branchname = 551) == 551
+    br3 = createbranch!(tree, n2, "Human")
+    @test getbranchname(tree, createbranch!(tree, nr, "Potato", name = 551)) == 551
     @test 551 ∈ branchroute(tree, "Dog", "Potato")
     deletebranch!(tree, 551)
-    br4 = addbranch!(tree, nr, "Potato")
-    @test Set(species) ⊆ Set(getdescendants(tree, nr))
+    createbranch!(tree, nr, "Potato")
+    @test Set(species) ⊆ Set(getnodename.(tree, getdescendants(tree, nr)))
     @test validate!(tree)
 end
 
