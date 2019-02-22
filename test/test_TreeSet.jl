092521450b12fb4e94bb82d86dd0ab28d5e33f4b
Richard Reeve <git@richardreeve.net>
2019-02-22 07:51:05 +0300

    Reincorporate JuliaDB, fix tests for TreeSets now they are not automatically generated by rand, and with filter requiring two arguments.

--- test/test_TreeSet.jl
+++ test/test_TreeSet.jl
@@ -2,7 +2,7 @@ module TestTreeSet
 
 using Phylo
 using DataFrames
-#using JuliaDB
+using JuliaDB
 
 using Compat.Test
 
@@ -10,48 +10,44 @@ species = ["Dog", "Cat", "Human"]
 ntips = 10
 df = DataFrame(species = species, count=[10, 20, 3])
 observations = ["Dog", "Cat", "Dog", "Dog"]
-#jdb = table(@NT(species = observations, count = [1, 2, 3, 4]))
-jdb = DataFrame(species = observations, count = [1, 2, 3, 4])
+@static if VERSION < v"0.7.0-"
+    jdb = table(@NT(species = observations, count = [1, 2, 3, 4]))
+else
+    jdb = table((species = observations, count = [1, 2, 3, 4]))
+end
 
-@testset "TestSet" begin
+@testset "TreeSet" begin
     @test length(rand(Ultrametric(ntips), 10)) ==
         length(rand(Nonultrametric(ntips), 10))
-    ts = rand(Ultrametric(ntips), 1:10)
-    @test length(treeiter(ts)) == length(treenameiter(ts)) == ntrees(ts) == 10
-    @test length(getbranchnames(ts)) == ntips * 2 - 2
-    for name in treenameiter(ts)
+    ts = TreeSet(rand(Ultrametric(ntips), 1:10))
+    @test length(gettrees(ts)) == length(gettreenames(ts)) == ntrees(ts) == 10
+    @test length(getleafnames(ts)) == ntips
+    for name in gettreenames(ts)
         @test name ∈ 1:10
+        @test length(getbranchnames(ts[name])) == ntips * 2 - 2
+        @test length(getbranchnames(ts)[name]) == ntips * 2 - 2
         @test length(getleafnames(ts[name])) == ntips
     end
-    for tree in treeiter(ts)
+    for tree in gettrees(ts)
         @test length(getnodenames(tree)) == ntips * 2 - 1
-    end
-
-    for tree in ts
         @test length(getbranchnames(tree)) == ntips * 2 - 2
         @test nleaves(tree) == ntips
     end
     @test nleaves(ts) == ntips
-    @test getleafnames(TreeSet(Dict{String, NamedTree}(), Dict{String, Any}())) == String[]
-    @test getbranchnames(TreeSet(Dict{String, NamedTree}(), Dict{String, Any}())) == Int[]
-    @test nleaves(rand(Ultrametric(df), 20)) == length(species)
-    @test nleaves(rand(Ultrametric(jdb), 10)) ==
+    @test getleafnames(TreeSet(Dict{String, NamedTree}(),
+                               Dict{String, Dict{String, Any}}())) == String[]
+    @test nleaves(TreeSet(rand(Ultrametric(df), 20))) == length(species)
+    @test nleaves(TreeSet(rand(Ultrametric(jdb), 10))) ==
         length(unique(observations))
     @test getleafinfo(rand(Ultrametric(df))) == df
-    @test getleafinfo(rand(Ultrametric(jdb), 2)) == jdb
-    @test nleaves(rand(Nonultrametric(df), 20)) == length(species)
-    @test nleaves(rand(Nonultrametric(jdb), 10)) ==
+    @test getleafinfo(TreeSet(rand(Ultrametric(jdb), 2))) == jdb
+    @test nleaves(TreeSet(rand(Nonultrametric(df), 20))) == length(species)
+    @test nleaves(TreeSet(rand(Nonultrametric(jdb), 10))) ==
         length(unique(observations))
     @test getleafinfo(rand(Nonultrametric(df))) == df
-    @test getleafinfo(rand(Nonultrametric(jdb), 2)) == jdb
+    @test getleafinfo(TreeSet(rand(Nonultrametric(jdb), 2))) == jdb
 
-    @test nodetype(ts) == nodetype(ts[1])
-    @test branchtype(ts) == branchtype(ts[1])
-    ts0 = rand(Nonultrametric(ntips), 0)
-    @test getleafnames(ts0) == String[]
-    @test getnodenames(ts0) == String[]
-    @test isempty(getbranchnames(ts0))
-    @test isempty(getleafinfo(ts0))
-    @test nleaves(ts0) == 0
+    @test nodetype(typeof(ts)) == nodetype(typeof(first(gettrees(ts))))
+    @test branchtype(typeof(ts)) == branchtype(typeof(first(gettrees(ts))))
 end
 end
