d0b12377d80e77bfd64cff0ac8db525dca360297
Richard Reeve <git@richardreeve.net>
2019-05-17 02:56:50 +0100

    Update newick and nexus testing to work with new tree types.

--- test/test_newick.jl
+++ test/test_newick.jl
@@ -3,52 +3,68 @@ module TestNewick
 using Phylo
 using Compat.Test
 
-@testset "A few simple binary trees" begin
-    @test length(nodeiter(parsenewick("((,),(,));"))) == 7
-    @test ["where", "when it's good", "Not mine", "MyLeaf"] ⊆
-        nodenameiter(parsenewick("""((MyLeaf,"when it's good"),
-                                     ('Not mine',where));"""))
-    tree = parsenewick("((MyLeaf[&Real=23,'Not real'={5,4}]:4.0,)Parent,(,));")
-    branches = branchfilter(tree) do tree, branch
-        return getnodename(tree, src(tree, branch)) == "Parent" &&
-            getnodename(tree, dst(tree, branch)) == "MyLeaf"
+@testset "newick and nexus parsing" begin
+    @testset "For $TreeType" for TreeType in [NamedBinaryTree]
+        @test length(nodeiter(parsenewick("((,),(,));"))) == 7
+        @test ["where", "when it's good", "Not mine", "MyLeaf"] ⊆
+            nodenameiter(parsenewick("""((MyLeaf,"when it's good"),
+                                             ('Not mine',where));""",
+                                     TreeType))
+        tree =
+            parsenewick("((MyLeaf[&Real=23,'Not real'={5,4}]:4.0,)Parent,(,));",
+                        TreeType)
+        branches = branchfilter(tree) do tree, branch
+            return getnodename(tree, src(tree, branch)) == "Parent" &&
+                getnodename(tree, dst(tree, branch)) == "MyLeaf"
+        end
+        @test length(branches) == 1
+        @test getlength(tree, first(branches)) ≈ 4.0
+        @test getnodedata(tree, "MyLeaf")["Real"] == 23
+        @test 5 ∈ getnodedata(tree, "MyLeaf")["Not real"]
+        @test 4 ∈ getnodedata(tree, "MyLeaf")["Not real"]
+        if VERSION < v"0.7.0-"
+            @test_warn "Tree ended but not finished" parsenewick("((,),(,));a")
+        end
+        @test_throws Exception parsenewick("((,),(,)));", TreeType)
+        @test_throws Exception parsenewick("((,),(,))", TreeType)
+        @test_throws Exception parsenewick("((,),(,);", TreeType)
+        @test_throws Exception parsenewick("((MyLeaf:-4.0,)Parent,(,));",
+                                           TreeType)
+        tree = open(f -> parsenewick(f, TreeType), Phylo.path("H1N1.newick"))
+        @test nleaves(tree) == 507
+        @test ntrees(tree) == 1
+        treex = open(f -> parsenexus(f, TreeType), Phylo.path("H1N1.trees"))
+        @test nleaves(tree) == nleaves(treex) ==
+            nleaves(treex["TREE1"]) == nleaves(treex["TREE2"])
+        @test ntrees(treex) == 2
     end
-    @test length(branches) == 1
-    @test getlength(tree, first(branches)) ≈ 4.0
-    @test getnodedata(tree, "MyLeaf")["Real"] == 23
-    @test 5 ∈ getnodedata(tree, "MyLeaf")["Not real"]
-    @test 4 ∈ getnodedata(tree, "MyLeaf")["Not real"]
-    if VERSION < v"0.7.0-"
-        @test_warn "Tree ended but not finished" parsenewick("((,),(,));a")
+    
+    @testset "For $TreeType" for TreeType in
+        [NamedTree, RootedTree, ManyRootTree]
+        @test nnodes(parsenewick("((,),(,,));", TreeType)) == 8
+        @test ["where", "when it's good", "Not mine", "MyLeaf", "next"] ⊆
+            nodenameiter(parsenewick("""((MyLeaf,"when it's good",next),
+                                             ('Not mine',where));""",
+                                     TreeType))
+        tree = parsenewick("((MyLeaf:4.0,)Parent,());", TreeType)
+        branches = branchfilter(tree) do tree, branch
+            return getnodename(tree, src(tree, branch)) == "Parent" &&
+                getnodename(tree, dst(tree, branch)) == "MyLeaf"
+        end
+        @test length(branches) == 1
+        @test getlength(tree, first(branches)) ≈ 4.0
+        @test_throws Exception parsenewick("((,),(,)));", TreeType)
+        @test_throws Exception parsenewick("((,),(,))", TreeType)
+        @test_throws Exception parsenewick("((,),(,);", TreeType)
+        tree = open(f -> parsenewick(f, TreeType), Phylo.path("H1N1.newick"))
+        @test nleaves(tree) == 507
+        @test ntrees(tree) == 1
+        treex = open(f -> parsenexus(f, TreeType), Phylo.path("H1N1.trees"))
+        @test nleaves(tree) == nleaves(treex) ==
+            nleaves(treex["TREE1"]) == nleaves(treex["TREE2"])
+        @test ntrees(treex) == 2
     end
-    @test_throws Exception parsenewick("((,),(,)));")
-    @test_throws Exception parsenewick("((,),(,))")
-    @test_throws Exception parsenewick("((,),(,);")
-    @test_throws Exception parsenewick("((MyLeaf:-4.0,)Parent,(,));")
-    tree = open(parsenewick, Phylo.path("H1N1.newick"))
-    @test nleaves(tree) == 507
-    @test ntrees(tree) == 1
-    treex = open(parsenexus, Phylo.path("H1N1.trees"))
-    @test nleaves(tree) == nleaves(treex) ==
-          nleaves(treex["TREE1"]) == nleaves(treex["TREE2"])
-    @test ntrees(treex) == 2
 end
 
-@testset "A few simple polytomous trees" begin
-    @test nnodes(parsenewick("((,),(,,));", NamedPolytomousTree)) == 8
-    @test ["where", "when it's good", "Not mine", "MyLeaf", "next"] ⊆
-        nodenameiter(parsenewick("""((MyLeaf,"when it's good",next),
-                                     ('Not mine',where));""", NamedPolytomousTree))
-    tree = parsenewick("((MyLeaf:4.0,)Parent,());", NamedPolytomousTree)
-    branches = branchfilter(tree) do tree, branch
-        return getnodename(tree, src(tree, branch)) == "Parent" &&
-            getnodename(tree, dst(tree, branch)) == "MyLeaf"
-    end
-    @test length(branches) == 1
-    @test getlength(tree, first(branches)) ≈ 4.0
-    @test_throws Exception parsenewick("((,),(,)));", NamedPolytomousTree)
-    @test_throws Exception parsenewick("((,),(,))", NamedPolytomousTree)
-    @test_throws Exception parsenewick("((,),(,);", NamedPolytomousTree)
 end
 
-end
