4c9ea835aaae301d8b36f2d2574253fbcc7855c8
Michael Krabbe Borregaard <mkborregaard@snm.ku.dk>
2021-01-13 19:33:06 +0100

    fix tests

--- test/test_metrics.jl
+++ test/test_metrics.jl
@@ -4,6 +4,7 @@ using Phylo
 using Test
 
 @testset "Metrics" begin
+    tree = open(parsenewick, Phylo.path("H1N1.newick"))
     species = getleaves(tree)[[2, 5, 8, 12, 22]];
     mrca = common_ancestor(tree, species)
     @test getnodename(tree, mrca) == "Node 65"
