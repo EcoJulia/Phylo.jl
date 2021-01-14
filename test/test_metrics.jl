dbaf1fd5e7a70fe19289b787e452da486e60e0d7
Michael Krabbe Borregaard <mkborregaard@snm.ku.dk>
2021-01-14 08:38:57 +0100

    fix tests

--- test/test_metrics.jl
+++ test/test_metrics.jl
@@ -5,31 +5,32 @@ using Test
 
 @testset "Metrics" begin
     trees = open(parsenexus, Phylo.path("H1N1.trees"))
+    tree1, tree2 = trees["TREE1"], trees["TREE2"]
     species = ["H1N1_A_PUERTORICO_8_1934", "H1N1_A_BRAZIL_11_1978"];
-    mrca1 = mrca(trees["TREE1"], species)
+    mrca1 = mrca(tree1, species)
 
     # MRCA should be the root
-    @test mrca1 == getnodename(tree["TREE1"], getroot(tree["TREE1"]))
-    nodes = getnodenames(tree["TREE1"])
-    desc1 = getdescendants(tree["TREE1"], mrca1)
+    @test mrca1 == getnodename(tree1, getroot(tree1))
+    nodes = getnodenames(tree1)
+    desc1 = getdescendants(tree1, mrca1)
 
     # MRCA + descendants is whole tree
     @test all(sort(nodes) .== sort(desc1 ∪ [mrca1]))
-    leaves = getleafnames(tree["TREE1"])
+    leaves = getleafnames(tree1)
     leaves1 = sort(leaves ∩ desc1)
 
     # All leaves are in descendants
     @test length(leaves1) == length(leaves)
 
     # All leaves are the same in both trees
-    mrca2 = mrca(trees["TREE2"], reverse(species))
-    leaves2 = sort(leaves ∩ getdescendants(tree["TREE2"], mrca2))
+    mrca2 = mrca(tree2, reverse(species))
+    leaves2 = sort(leaves ∩ getdescendants(tree2, mrca2))
     @test all(leaves1 .== leaves2)
 
     # MRCA for Sendai strains has 8 descendants, including Fukuoka
     desendais =
-        getdescendants(tree["TREE1"],
-                       mrca(tree["TREE1"],
+        getdescendants(tree1,
+                       mrca(tree1,
                                        filter(x -> contains(x, "SENDAI"),
                                        leaves)))
     @test length(desendais) == 8
