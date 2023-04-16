6bf55c66301a86ed25504c8a5d19837f0653375b
richardreeve <richard.reeve@glasgow.ac.uk>
2023-04-16 22:47:57 +0100

    Fix inference testing

--- test/run_inference.jl
+++ test/run_inference.jl
@@ -1,4 +1,4 @@
-module TestInference
+module TestInference_wrapped
 
 using Test
 using Phylo
@@ -17,17 +17,23 @@ global skipR = !rcopy(R"require(phylolm)")
     jtree = open(parsenewick, Phylo.path("hummingbirds.tree"))
     R"rtree = ape::read.tree(file = 'hummingbirds.tree')" #I dont think this is the way I should load in the tree to R. Tried like in other files but I couldnt get it to load into phylolm.
 
-    #Create dataframe with leafnames and random trait values
+    # Create dataframe with leafnames and random trait values
     species = getleafnames(jtree)
     data = 1000 .* rand(length(species))
     dat = DataFrame(species = species, data = data)
     @rput dat
 
-    #species need to be the row names in R
+    # Save data on leaves so can test on estimaterates 2
+    setnodedata!.(jtree, species, "trait", data)
+
+    # Species need to be the row names in R
     R"row.names(dat) <- dat$species"
 
     rfit = rcopy(R"phylolm(data~1,data=dat,phy=rtree)")
-    jfit = estimaterates(jtree, dat)
+    jfit = estimaterates(jtree, "trait")
+
+    species = getleaves(jtree)
+    data = 1000 .* rand(length(species))
 
     @test rfit[:coefficients] ≈ jfit[1]
     @test rfit[:sigma2] ≈ jfit[2]
