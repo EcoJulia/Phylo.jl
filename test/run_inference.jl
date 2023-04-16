6e50f57a94a9f8f4d2e35ed1d30c4f3d3ce4405d
richardreeve <richard.reeve@glasgow.ac.uk>
2023-04-16 23:31:32 +0100

    Only compare to R if packages are installed

--- test/run_inference.jl
+++ test/run_inference.jl
@@ -5,39 +5,40 @@ using Phylo
 using RCall
 using DataFrames
 
-global skipR = !rcopy(R"require(ape)")
-global skipR = !rcopy(R"require(phylolm)")
+global skipR = !(rcopy(R"require(ape)") && rcopy(R"require(phylolm)"))
 
 @testset "Compare estimaterates output to phylolm" begin
-    
-    R"
-    library('ape')
-    library('phylolm')"
-
-    jtree = open(parsenewick, Phylo.path("hummingbirds.tree"))
-    R"rtree = ape::read.tree(file = 'hummingbirds.tree')" #I dont think this is the way I should load in the tree to R. Tried like in other files but I couldnt get it to load into phylolm.
+    jtree = nothing
+    @test_nowarn jtree = open(parsenewick, Phylo.path("hummingbirds.tree"))
 
     # Create dataframe with leafnames and random trait values
     species = getleafnames(jtree)
     data = 1000 .* rand(length(species))
     dat = DataFrame(species = species, data = data)
-    @rput dat
 
     # Save data on leaves so can test on estimaterates 2
     setnodedata!.(jtree, species, "trait", data)
 
-    # Species need to be the row names in R
-    R"row.names(dat) <- dat$species"
-
-    rfit = rcopy(R"phylolm(data~1,data=dat,phy=rtree)")
-    jfit = estimaterates(jtree, "trait")
+    jfit = nothing
+    @test_nowarn jfit = estimaterates(jtree, "trait")
 
     species = getleaves(jtree)
     data = 1000 .* rand(length(species))
 
-    @test rfit[:coefficients] ≈ jfit[1]
-    @test rfit[:sigma2] ≈ jfit[2]
-    @test rfit[:logLik] ≈ -jfit[3]
+    if !skipR
+        rtree = rcall(Symbol("read.tree"), Phylo.path("hummingbirds.tree"))
+        @rput rtree
+        @rput dat
+
+        # Species need to be the row names in R
+        R"row.names(dat) <- dat$species"
+
+        rfit = rcopy(R"phylolm(data ~ 1, data = dat, phy = rtree)")
+
+        @test rfit[:coefficients] ≈ jfit[1]
+        @test rfit[:sigma2] ≈ jfit[2]
+        @test rfit[:logLik] ≈ -jfit[3]
+    end
 end
 
 end
