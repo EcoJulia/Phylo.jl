18c9b0fec6f0921fc4d8c2343fb2a888c80accd1
richardreeve <richard.reeve@glasgow.ac.uk>
2023-12-02 02:55:04 +0000

    More plot testing

--- test/test_plot.jl
+++ test/test_plot.jl
@@ -6,9 +6,13 @@ using Plots
 
 @testset "Plots" begin
     tree = open(parsenewick, Phylo.path("hummingbirds.tree"))
-    @test plot(tree).n == 1
+    @test length(plot(tree).subplots) == 1
     trait = map_depthfirst((val, node) -> val + randn(), 0., tree, Float64)
     @test plot(tree, treetype = :fan, line_z = trait, linecolor = :RdYlBu, linewidth = 5, showtips = false).n == 1
+    @test plot(tree,
+        markersize = 10, markercolor = :steelblue, markerstrokecolor = :white,
+        series_annotations = text.(1:nnodes(tree), 5, :center, :center, :white,
+        tipfont = (4,))).init
 end
 
 end
