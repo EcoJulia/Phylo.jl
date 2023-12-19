42bddd00dfad4fb9158c78b6f744e524d38c5840
richardreeve <richard.reeve@glasgow.ac.uk>
2023-12-19 00:46:58 +0000

    Testing node and branch outputs

--- test/test_show.jl
+++ test/test_show.jl
@@ -21,26 +21,29 @@ a = IOBuffer()
     @test_nowarn show(a, first(branchiter(nt)))
     ts = rand(Nonultrametric{TreeType}(10), 10)
     @test_nowarn show(a, ts)
-    ps = parsenewick("((,),(,,));", TreeType)
-    @test_nowarn show(a, ps)
-    @test_nowarn show(a, [ps])
-    @test_nowarn show(a, first(nodeiter(ps)))
-    @test_nowarn show(a, first(branchiter(ps)))
-    @test_nowarn show(a, (tree=ps, node=first(getnodes(ps))))
-    @test_nowarn show(a, (tree=ps, node=getparent(ps, first(getleaves(ps)))))
-    @test_nowarn show(a, (tree=ps, branch=first(getbranches(ps))))
-    b = IOBuffer()
-    @test_nowarn show(IOContext(b, :compact => false), ps)
-    @test Phylo.outputtree(ps, Phylo.StandardOutput()) == String(take!(b))
-    @test_nowarn show(IOContext(b, :compact => true), ps)
-    @test Phylo.outputtree(ps, Phylo.CompactOutput()) == String(take!(b))
-    @test Phylo.outputnode(ps, first(getnodes(ps)), Phylo.CompactOutput()) isa String
-    @test Phylo.outputnode(ps, first(getnodes(ps)), Phylo.StandardOutput()) isa String
-    @test Phylo.outputbranch(ps, first(getbranches(ps)), Phylo.CompactOutput()) isa String
-    @test Phylo.outputbranch(ps, first(getbranches(ps)), Phylo.StandardOutput()) isa String
-    if TreeType == RootedTree
-        @test_nowarn show(a, getnodes(ps))
-        @test_nowarn show(a, getbranches(ps))
+    @testset "$newick" for newick in 
+        ["((A,B),(,,));", "((,),(A[&a=\"aaa\", b=1],,));", "((,):3.2,(,,):[&a=\"aaa\", b=1]23.2);"]
+        ps = parsenewick(newick, TreeType)
+        @test_nowarn show(a, ps)
+        @test_nowarn show(a, [ps])
+        @test_nowarn show(a, first(nodeiter(ps)))
+        @test_nowarn show(a, first(branchiter(ps)))
+        @test_nowarn show(a, (tree=ps, node=first(getnodes(ps))))
+        @test_nowarn show(a, (tree=ps, node=getparent(ps, first(getleaves(ps)))))
+        @test_nowarn show(a, (tree=ps, branch=first(getbranches(ps))))
+        b = IOBuffer()
+        @test_nowarn show(IOContext(b, :compact => false), ps)
+        @test Phylo.outputtree(ps, Phylo.StandardOutput()) == String(take!(b))
+        @test_nowarn show(IOContext(b, :compact => true), ps)
+        @test Phylo.outputtree(ps, Phylo.CompactOutput()) == String(take!(b))
+        @test Phylo.outputnode(ps, first(getnodes(ps)), Phylo.CompactOutput()) isa String
+        @test Phylo.outputnode(ps, first(getnodes(ps)), Phylo.StandardOutput()) isa String
+        @test Phylo.outputbranch(ps, first(getbranches(ps)), Phylo.CompactOutput()) isa String
+        @test Phylo.outputbranch(ps, first(getbranches(ps)), Phylo.StandardOutput()) isa String
+        if TreeType == RootedTree
+            @test_nowarn show(a, getnodes(ps))
+            @test_nowarn show(a, getbranches(ps))
+        end
     end
 end
 end
